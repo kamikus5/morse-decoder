e98342025cbaf065533ea1ed02c65007738438ba
kamikus5 <55898991+kamikus5@users.noreply.github.com>
2020-10-13 22:37:41 +0300

    init: morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let index = 0;
+	let arrayNumber = [];
+	
+    for (let i = 0; i <= expr.length; i++) {	
+    	if (i > 0 && i % 10 === 0) {
+    	let subStr = expr.slice(index, i);
+    	let io = subStr.indexOf(1);
+    	let str = subStr.slice(io);
+    	
+    	arrayNumber.push(str);
+    		index += 10;
+    	}
+    }
+    
+    for (let i = 0; i < arrayNumber.length; i++) {
+    	for (let k = 0; k < arrayNumber[i].length; k++) {
+    	
+    	arrayNumber[i] = arrayNumber[i].replace('10', '.');
+    	arrayNumber[i] = arrayNumber[i].replace('11', '-');
+    	}
+    	arrayNumber[i] = arrayNumber[i].replace('*', ' ');
+    }
+    
+    for (let symbol in MORSE_TABLE) {
+    	for (let i = 0; i < arrayNumber.length; i++) {
+    		if (symbol === arrayNumber[i]) {
+    		arrayNumber[i] = MORSE_TABLE[symbol];
+    	    }
+    	}
+    }
+		let word = arrayNumber.join('');
+		return word;
 }
 
 module.exports = {
